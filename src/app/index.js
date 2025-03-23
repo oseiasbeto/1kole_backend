36856ac857c0fbad2400a47781addcb50c21aa67
Oséias BC <oseiasbetodev@gmail.com>
2025-03-23 22:33:45 +0100

    Verifiquei o erro 3

--- src/app/index.js
+++ src/app/index.js
@@ -10,10 +10,8 @@ const connectDB = require('./config/connectDb'); // Função para conectar ao ba
 connectDB(); // Estabelece a conexão com o banco de dados
 
 // Configurando os middlewares
-app.use(cors()); // Habilita o CORS para que o servidor aceite requisições de outros domínios
-// Middleware para permitir uploads grandes
-app.use(express.json({ limit: '100mb' })); // Aumenta o limite para JSON
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(cors());
+app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads"))); // Serve arquivos estáticos da pasta "uploads" através da rota "/files"
 
 // Importando as rotas do aplicativo
